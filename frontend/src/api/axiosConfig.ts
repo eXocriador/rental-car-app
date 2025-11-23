545ccbf019d3176eab41a229b518b18ceea3b65b
Oleh Tatarynov <exocriador@Olehs-MacBook-Pro.local>
2025-11-23 14:52:05 +0200

    minor fix

--- frontend/src/api/axiosConfig.ts
+++ frontend/src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios, { InternalAxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "http://localhost:4000/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json"
